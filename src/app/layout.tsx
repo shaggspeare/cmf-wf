7f1de9edb042b3adb2c1d40a06a8cf063a4feec9
viach <viacheslav.stetsenko@gmail.com>
2025-09-10 21:59:26 +0300

    add contents.md change layout to be Next js like, change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,31 @@
 import type { Metadata } from "next";
-// import "./globals.css";
+import { Manrope, Inter } from "next/font/google";
 import "./css/styles.css";
-import Script from "next/script";
+
+const manrope = Manrope({
+  subsets: ["latin", "cyrillic-ext"],
+  weight: ["300", "500", "800"],
+  variable: "--font-manrope",
+  display: "swap",
+});
+
+const inter = Inter({
+  subsets: ["latin", "cyrillic-ext"],
+  weight: ["300", "500", "800"],
+  variable: "--font-inter",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
-  title: "M.Philips - Webflow Ecommerce website template",
-  description: "A seasoned UI/UX designer dedicated to bridging creativity with user-centric design principles",
+  title: "Valeria Vari - CMF course",
+  description:
+    "The course is an author's program in furniture design and CMF Design by Valeria Vari, starting September 25, 2025",
   openGraph: {
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
   },
-  viewport: "width=device-width, initial-scale=1",
   generator: "Next.js",
 };
 
@@ -22,38 +35,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      data-wf-page="68089cd72439612a4ce1e319"
-      data-wf-site="68089cd72439612a4ce1e2e4"
-    >
-      <head>
-        <link href="https://fonts.googleapis.com" rel="preconnect" />
-        <link
-          href="https://fonts.gstatic.com"
-          rel="preconnect"
-          crossOrigin="anonymous"
-        />
-      </head>
-      <body>
-        <Script
-          src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"
-          strategy="beforeInteractive"
-        />
-        <Script id="webfont-loader" strategy="beforeInteractive">
-          {`
-            WebFont.load({
-              google: {
-                families: [
-                  "Poppins:300,regular,italic,500,600,700,800,900",
-                  "Inter:300,regular,500,600,700,800,900,italic",
-                ],
-              },
-            });
-          `}
-        </Script>
-        {children}
-      </body>
+    <html lang="en" className={`${manrope.variable} ${inter.variable}`}>
+      <body className={manrope.className}>{children}</body>
     </html>
   );
 }
